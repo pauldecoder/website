5a73a1567e174b820d33c21eb08714b0116bc8b0
pauldecoder <pauldevinemail@gmail.com>
2026-08-02 19:03:19 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,42 @@
+export const CONFIG = {
 
+    appName:
+        "PD PLAYLIST LIBRARY",
+
+
+    accentColor:
+        "#2F80FF",
+
+
+    artworkSize:
+        300,
+
+
+    visualizerBars:
+        48,
+
+
+    preloadNextTrack:
+        true,
+
+
+    enableGapless:
+        true,
+
+
+    enableMediaSession:
+        true,
+
+
+    enableStatistics:
+        true,
+
+
+    enablePWA:
+        true,
+
+
+    defaultArtwork:
+        "./assets/artwork/default-cover.png"
+
+};
