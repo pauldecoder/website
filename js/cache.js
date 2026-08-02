fb4f16f7673a34950ff6b05efa55ad48b3a0595c
pauldecoder <pauldevinemail@gmail.com>
2026-08-02 19:08:02 -0400

    Update cache.js

--- js/cache.js
+++ js/cache.js
@@ -1,1 +1,30 @@
+export async function preloadAudio(
+    url
+) {
 
+
+    try {
+
+
+        await fetch(
+            url,
+            {
+                mode:
+                "no-cors"
+            }
+        );
+
+
+    }
+    catch(error) {
+
+
+        console.warn(
+            "Preload failed:",
+            error
+        );
+
+
+    }
+
+}
