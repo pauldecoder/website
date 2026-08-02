4483186a4b7b7b5a9533e4a26b7128821c274d08
pauldecoder <pauldevinemail@gmail.com>
2026-08-02 19:04:50 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,54 @@
+import {
+    loadLibrary
+}
+from "./library.js";
 
+import {
+    initializePlayer
+}
+from "./player.js";
+
+import {
+    initializeVisualizer
+}
+from "./visualizer.js";
+
+import {
+    initializeMediaSession
+}
+from "./media-session.js";
+
+
+
+async function startApp() {
+
+
+    await loadLibrary();
+
+
+    initializePlayer();
+
+
+    initializeVisualizer();
+
+
+    initializeMediaSession();
+
+
+
+    if (
+        "serviceWorker" in navigator
+    ) {
+
+        navigator.serviceWorker.register(
+            "./service-worker.js"
+        );
+
+    }
+
+
+}
+
+
+
+startApp();
